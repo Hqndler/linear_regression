2bb726f0affcaaed32dd260d5745aa71012e7d0c
handler <69089935+Hqndler@users.noreply.github.com>
2024-11-26 14:30:58 +0100

    numpyzation + mse

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,6 @@
 import sys, os
 import pandas as pd
+from math import sqrt
 
 class LinearRegression:
     t0 = 0
@@ -18,26 +19,25 @@ def estimate_price(mileage : float):
 
 def get_data() -> dict:
     data : dict = pd.read_csv("data.csv")
-    km = list(map(lambda x: float(x), list(data['km'])))
-    price = list(map(lambda x: float(x), list(data['price'])))
+    km = data['km']
+    price = data['price']
     
     if len(price) != len(km):
         print("Something went wrong with the csv file. Please check all line are populated")
     return km, price
 
 def gradient0(km : list, price : list) -> float:
-    return lr.lr * (sum([estimate_price(km[i]) - price[i] for i, _ in enumerate(km)]) / len(km))
+    return lr.lr * sum(estimate_price(km) - price) / len(km)
 
 def gradient1(km : list, price : list) -> float:
-    return lr.lr * (sum([(estimate_price(km[i]) - price[i]) * km[i] for i, _ in enumerate(km)]) / len(km))
+    return lr.lr * (sum((estimate_price(km) - price) * km) / len(km))
 
 def standardize(km, price):
     lr.min_k, lr.max_k = min(km), max(km)
     lr.min_p, lr.max_p = min(price), max(price)
 
-    for i, _ in enumerate(km):
-        km[i] = (km[i] - lr.min_k) / (lr.max_k - lr.min_k)
-        price[i] = (price[i] - lr.min_p) / (lr.max_p - lr.min_p)
+    km = (km - lr.min_k) / (lr.max_k - lr.min_k)
+    price = (price - lr.min_p) / (lr.max_p - lr.min_p)
 
     return km, price
 
@@ -45,15 +45,19 @@ def main():
     km, price = standardize(*get_data())
     epoch = 0
 
-    while epoch != 3000:
+    while epoch != 1000:
         lr.t0 -= gradient0(km, price)
         lr.t1 -= gradient1(km, price)
         epoch += 1
-
+    
     lr.t1 = (lr.max_p - lr.min_p) * lr.t1 / (lr.max_k - lr.min_k)
     lr.t0 = lr.min_p + ((lr.max_p - lr.min_p) * lr.t0) + lr.t1 * (1 - lr.min_k)
 
     print(lr)
+    
+    std_km, std_price = get_data()
+    mse = sqrt(sum(((estimate_price(std_km) - std_price) ** 2)) / len(std_km))
+    print(f"Precision : +/- {mse:.3f}")
 
     with open("theta.csv", "w") as file:
         file.write(f"t0,t1\n{lr.t0},{lr.t1}")
